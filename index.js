3581009f02de39270b393bdf2f0df8e80230c50a
beaulazear <beaulazear@gmail.com>
2022-09-24 00:30:10 -0400

    commit tree 7094c80971b0122efa4a6272ea8315a094f1d811

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function saturdayFun(target = "roller-skate") {
+    return (`This Saturday, I want to ${target}!`);
+}
+saturdayFun();
+
+function mondayWork(targetTwo = "go to the office") {
+    return (`This Monday, I will ${targetTwo}.`)
+}
+mondayWork();
+
+function wrapAdjective(paramTwo = "*") {
+    return function (param ="special") {
+        return `You are ${paramTwo}${param}${paramTwo}!`;
+    }
+}
